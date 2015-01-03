842f1e162293696ae05f4a1133dbd027044dc973
Alex Stoev <alex.stoev@gmail.com>
2015-01-03 20:22:42 +0200

    Added ngRouter and home view

--- app/MoneyFlow.js
+++ app/MoneyFlow.js
@@ -1,1 +1,12 @@
-angular.module('moneyFlow', ['session', 'navigation']);
+angular.module('moneyFlow', ['ngRoute', 'session', 'navigation', 'home'])
+.config(['$routeProvider',function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'HomeController',
+			templateUrl: '/home/partials/home.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+
+}]);
