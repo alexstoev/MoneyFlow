b3655a899218e0bb348133aa9160af22d1046530
Alex Stoev <alex.stoev@gmail.com>
2015-01-03 17:34:59 +0200

    Improve the server to be able to serve static from multiple folders

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
-app.use(express.static(__dirname + '/..' + '/app')); // we'll serve just stastic resoures for now
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use('/', express.static(__dirname + '/..' + '/app')); // we'll serve just stastic resoures for now
+app.use('/bower_components', express.static(__dirname + '/..' + '/bower_components')); // we'll serve just stastic resoures for now
 
 // start the server
 var server = app.listen(3000);
